8da953b6470bbe73b68974d6def98d56e119c77f
Pablo Aguilar <163502@ids.upchiapas.edu.mx>
2019-04-01 23:29:39 -0600

    photo prueba 1

--- usersLobby/urls.py
+++ usersLobby/urls.py
@@ -5,6 +5,6 @@ from usersLobby.views import *
 urlpatterns=[
     re_path(r'^$',UsersLobbyView.as_view(), name='UsersLobby'),
     re_path(r'^edit/(?P<pk>\d+)$', UsersLobbyEdit.as_view(), name='UsersLobby'),
-    re_path(r'^data/list/(?P<id>\d+)$', UsersLobbyList.as_view(), name='UsersLobby1')
+    re_path(r'^data/list/(?P<id>\d+)$', UsersLobbyList.as_view(), name='UsersLobby1'),
     re_path(r'^data/photo/(?P<id>\d+)/(?P<id>\d+)$', UsersLobbyByUserANDLobby.as_view(), name='UsersLobbyPhoto')
 ]
