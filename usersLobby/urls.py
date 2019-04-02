3a3531ae3fe506241f374363a9cf3fbc2aec0817
Pablo Aguilar <163502@ids.upchiapas.edu.mx>
2019-04-01 23:26:36 -0600

    getLobbyLike

--- usersLobby/urls.py
+++ usersLobby/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
     re_path(r'^$',UsersLobbyView.as_view(), name='UsersLobby'),
     re_path(r'^edit/(?P<pk>\d+)$', UsersLobbyEdit.as_view(), name='UsersLobby'),
     re_path(r'^data/list/(?P<id>\d+)$', UsersLobbyList.as_view(), name='UsersLobby1')
-    re_path(r'^data/photo/(?P<id>\d+)/(?P<id>\d+)$', UsersLobbyList.as_view(), name='UsersLobbyPhoto')
+    re_path(r'^data/photo/(?P<id>\d+)/(?P<id>\d+)$', UsersLobbyByUserANDLobby.as_view(), name='UsersLobbyPhoto')
 ]
